e4850d5f6e2f8f4cb9523476ebb15c67aef6f028
truongne <truonggolang@gmail.com>
2023-10-10 17:19:24 +0700

    [swagger] 4.15.5

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,17 @@ async function bootstrap() {
     .addBearerAuth()
     .build()
   const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup(process.env.SERVER_NAME, app, document)
+  SwaggerModule.setup(process.env.SERVER_NAME, app, document, {
+    customfavIcon: 'https://cdn.icon-icons.com/icons2/2107/PNG/512/file_type_swagger_icon_130134.png',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
+    ],
+    customCssUrl: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
+    ],
+  })
   //endSwagger
 
   app.useGlobalPipes(new ValidationPipe())
